828f9e8119ce97efeec17ab7ea6f2bc4a71232d8
MandlikSanika18 <mandliksanika18@.com>
2025-07-25 12:53:14 +0530

    dist

--- Backend/index.js
+++ Backend/index.js
@@ -2,14 +2,21 @@ const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
+const path = require('path');
+
 
 dotenv.config();
 connectDB();
 
 const app = express();
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../Frontend/dist')));
 
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../Frontend/dist','index.html'));                                                
+
+});
 app.use('/api/auth', authRoutes);
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
